de03e66e8cba4428bf18f18b96d90654e6bee471
ZhaoLinlin <linlin.zhao@mxplayer.in>
2021-04-26 11:10:25 +0800

    New test images

--- app/src/main/java/com/next/easyloader/MainActivity.kt
+++ app/src/main/java/com/next/easyloader/MainActivity.kt
@@ -26,20 +26,21 @@ class MyAdapter(private val activity: AppCompatActivity) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val arrayOf = arrayOf(
-            "file:///sdcard/test/test.jpeg",
-            "file:///sdcard/test/test7.gif",
             "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201611%2F04%2F20161104110413_XzVAk.thumb.700_0.gif&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621752418&t=e2f8cbf44ad8616d6c4bf936b5713400",
+            "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.yao51.com%2Fjiankangtuku%2Fodogpboiz.jpeg&refer=http%3A%2F%2Fimg.yao51.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621998326&t=2e79153eba2d13d1f36a0709f9b42cc0",
             "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi2.sinaimg.cn%2Fty%2Fk%2Fp%2F2009-10-28%2FU3144P6T12D4667860F168DT20091028123558.jpg&refer=http%3A%2F%2Fi2.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621996238&t=df3110fb17db54c3d030395c8f509091",
             "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage-7.verycd.com%2Fccb3f249509bcaac82dd4cb6ef5a7257104641%2F5.jpg&refer=http%3A%2F%2Fimage-7.verycd.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621996264&t=e1870dbccce3514561131144953e53d2",
-            "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fycyima.yccar.com%2FUploadfile%2Fother%2F2011%2F01%2F11%2F106612718.jpg&refer=http%3A%2F%2Fycyima.yccar.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621996282&t=e7c986421df6dceb70ab4e8b688ce2bf"
+            "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fycyima.yccar.com%2FUploadfile%2Fother%2F2011%2F01%2F11%2F106612718.jpg&refer=http%3A%2F%2Fycyima.yccar.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621996282&t=e7c986421df6dceb70ab4e8b688ce2bf",
+            "https://gimg2.baidu.com/image_search/src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180510%2Fc861c0e9509546f98c25ef09419f1b81.gif&refer=http%3A%2F%2F5b0988e595225.cdn.sohucs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621998484&t=aedb85b4a7a7199419f0914da9dcf903",
+            "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.17qq.com%2Fimg_biaoqing%2F82468534.jpeg&refer=http%3A%2F%2Fwww.17qq.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621998526&t=86af106a88f814b7c1f7448cf6ac90d0",
+            "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.17qq.com%2Fimg_biaoqing%2F22426019.jpeg&refer=http%3A%2F%2Fwww.17qq.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621998551&t=57129c8e0b2b87428393fe0f094e34e6"
         )
-        val elapsedRealtime = SystemClock.elapsedRealtime()
 
-        val nextInt = elapsedRealtime % 6
+        val nextInt = position % arrayOf.size
 
         EasyImage
             .with(activity)
-            .load(arrayOf[nextInt.toInt()])
+            .load(arrayOf[nextInt])
             .placeholder(R.drawable.dark_mode_icon)
             .error(R.drawable.dailycheck_dialog)
             .fadeIn()
